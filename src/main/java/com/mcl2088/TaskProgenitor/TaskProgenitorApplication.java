ad04df91962ea9e0a9c714c98054a72a43ac3615
Xiaolequan <stevenli2088@hotmail.com>
2024-09-18 16:47:45 -0700

    Initial commit

---
+++ src/main/java/com/mcl2088/TaskProgenitor/TaskProgenitorApplication.java
@@ -0,0 +1,13 @@
+package com.mcl2088.TaskProgenitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProgenitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskProgenitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcl2088/TaskProgenitor/TaskProgenitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcl2088.TaskProgenitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProgenitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
