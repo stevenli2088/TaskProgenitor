c1dd6c58f1c0290cf1f20b97ec70e15f3486b79e
Steven <stevenli2088@hotmail.com>
2024-10-04 01:27:01 -0700

    Edited TaskList and Card

--- TaskProgenitorFrontEnd/src/components/TaskCard.tsx
+++ TaskProgenitorFrontEnd/src/components/TaskCard.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import { Task } from '../models/task'
-
+import React from 'react';
+import { Task } from '../models/task';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import { MdDone } from 'react-icons/md';
+import "./styles.css";
 type Props = {
-    task:Task,
+    task: Task,
     tasks: Task[],
     setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-const TaskCard = (task, tasks, setTasks) => {
+const TaskCard: React.FC<Props> = ({task, tasks, setTasks}) => {
   return (
-    <div>TaskCard</div>
+    <form className="task__card">
+      <span className="task__card--text">{task.taskName}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+
+    </form>
   )
 }
 
